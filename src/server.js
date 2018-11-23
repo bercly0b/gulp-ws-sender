e3b6dad48bc167b7d52c87cd957094326b406a22
bercly0b <vvworkmail@yandex.ru>
2018-11-23 21:58:48 +0300

    fixed client saving error

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const WebSocket = require('ws')
-const chalk = require('chalk')
 const getMessage = require('./helper').getServerMessage
 
 module.exports = port => {
@@ -11,18 +10,18 @@ module.exports = port => {
 
   webSocketServer.on('connection', ws => {
 
-    const client = clients.gulp ? 'browser' : 'gulp-ws-sender'
+    const client = clients.sender ? 'browser' : 'sender'
     clients[client] = ws
     console.log(getMessage('Connected', client))
-
+    
     ws.on('close', () => {
       delete clients[client]
       console.log(getMessage('Disconnected', client))
     })
 
-    if (needHandler && clients.gulp) {
+    if (needHandler && clients.sender) {
       needHandler = false
-      clients.gulp.on('message', message => {
+      clients.sender.on('message', message => {
         // console.log(getMessage('Received message from', client), `: ${message}`)
         clients.browser && clients.browser.send(message)
       })
